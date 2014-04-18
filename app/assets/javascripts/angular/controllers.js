2bce7a624ff983442929b5c97df4a1768012668c
Joshua Lam <joh641@gmail.com>
2014-04-18 00:21:15 -0700

    disable infinite scroll for now

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -7,15 +7,15 @@ var littledipperControllers = angular.module('littledipperControllers', []);
 littledipperControllers.controller('CourseListCtrl', ['$scope', 'Course',
   function($scope, Course) {
     $scope.courses = Course.query();
-    $scope.displayedCourses = [];
+    // $scope.displayedCourses = [];
    
-    $scope.addMoreCourses = function() {
-      var start = $scope.displayedCourses.length
-      var last = $scope.courses.length - 1
-      for(var i = start; i <= start + 25 && i <= last; i++) {
-        $scope.displayedCourses.push($scope.courses[i]);
-      }
-    };
+    // $scope.addMoreCourses = function() {
+    //   var start = $scope.displayedCourses.length
+    //   var last = $scope.courses.length - 1
+    //   for(var i = start; i <= start + 30 && i <= last; i++) {
+    //     $scope.displayedCourses.push($scope.courses[i]);
+    //   }
+    // };
   }]);
 
 littledipperControllers.controller('CourseDetailCtrl', ['$scope', '$routeParams', 'Course',
